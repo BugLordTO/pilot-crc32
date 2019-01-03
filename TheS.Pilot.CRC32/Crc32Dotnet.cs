812aab431b90acbf79973ba9cc41b3719c5ae883
Somkiet <somsor@perfenterprise.com>
2019-01-03 14:05:01 +0700

    test

---
+++ TheS.Pilot.CRC32/Crc32Dotnet.cs
@@ -0,0 +1,13 @@
+﻿namespace TheS.Pilot.CRC32
+{
+    /// <summary>
+    /// NUGET: https://github.com/force-net/Crc32.NET/blob/develop/Crc32.NET.Tests/ImplementationTest.cs
+    /// </summary>
+    public class Crc32Dotnet
+    {
+        public uint Get(byte[] bytes)
+        {
+            return Force.Crc32.Crc32Algorithm.Compute(bytes);
+        }
+    }
+}
---
+++ TheS.Pilot.CRC32/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheS.Pilot.CRC32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
