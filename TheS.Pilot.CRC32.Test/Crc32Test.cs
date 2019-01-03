5efdce8353c76f573b320c6a04d420edcefe3d34
Somkiet <somsor@perfenterprise.com>
2019-01-03 14:09:46 +0700

    test case for utf8

--- TheS.Pilot.CRC32.Test/Crc32Test.cs
+++ TheS.Pilot.CRC32.Test/Crc32Test.cs
@@ -10,14 +10,13 @@ namespace TheS.Pilot.CRC32.Test
         [InlineData("Hello world.", 0x8b3a0404)]
         [InlineData("The quick brown fox jumps over the lazy dog", 0x414FA339)]
         [InlineData("CRC is one of the most reliable error detection schemes and can detect up to 95.5% of all errors. The most commonly used code is the CRC-32 standard code which is defined by the CCITT, and will give a 32-bit CRC signature (8 hex characters). This signature is normally appended onto the data, and then checked when the data is read. If the CRC-32 check differs from the stored value, there is likely to be an error in the data.", 0x75247dbf)]
-        [InlineData("ÊÇÑÊ´Õ¤¹ä·Â", 0x05399b9e)]
-        [InlineData("¡¢¤§", 0x48839247)]
-        [InlineData("¢¤§¨", 0x48839247)]
+        [InlineData("ÊÇÑÊ´Õ¤¹ä·Â", 0x5fc1be2c)]
+        [InlineData("¡¢¤§", 0x5d8914f4)]
+        [InlineData("¢¤§¨", 0x576428b2)]
         public void Test1(string text, uint expected)
         {
             var sut = new Crc32Sanity();
-            //var bytes = Encoding.UTF8.GetBytes(text);
-            var bytes = Encoding.ASCII.GetBytes(text);
+            var bytes = Encoding.UTF8.GetBytes(text);
             var actual = sut.ComputeChecksum(bytes);
             Assert.Equal(expected, actual);
         }
@@ -26,14 +25,13 @@ namespace TheS.Pilot.CRC32.Test
         [InlineData("Hello world.", 0x8b3a0404)]
         [InlineData("The quick brown fox jumps over the lazy dog", 0x414FA339)]
         [InlineData("CRC is one of the most reliable error detection schemes and can detect up to 95.5% of all errors. The most commonly used code is the CRC-32 standard code which is defined by the CCITT, and will give a 32-bit CRC signature (8 hex characters). This signature is normally appended onto the data, and then checked when the data is read. If the CRC-32 check differs from the stored value, there is likely to be an error in the data.", 0x75247dbf)]
-        [InlineData("ÊÇÑÊ´Õ¤¹ä·Â", 0x05399b9e)]
-        [InlineData("¡¢¤§", 0x48839247)]
-        [InlineData("¢¤§¨", 0x48839247)]
+        [InlineData("ÊÇÑÊ´Õ¤¹ä·Â", 0x5fc1be2c)]
+        [InlineData("¡¢¤§", 0x5d8914f4)]
+        [InlineData("¢¤§¨", 0x576428b2)]
         public void Test2(string text, uint expected)
         {
             var sut = new Crc32Rosetta();
-            //var bytes = Encoding.UTF8.GetBytes(text);
-            var bytes = Encoding.ASCII.GetBytes(text);
+            var bytes = Encoding.UTF8.GetBytes(text);
             var actual = sut.Get(bytes);
             Assert.Equal(expected, actual);
         }
@@ -42,14 +40,13 @@ namespace TheS.Pilot.CRC32.Test
         [InlineData("Hello world.", 0x8b3a0404)]
         [InlineData("The quick brown fox jumps over the lazy dog", 0x414FA339)]
         [InlineData("CRC is one of the most reliable error detection schemes and can detect up to 95.5% of all errors. The most commonly used code is the CRC-32 standard code which is defined by the CCITT, and will give a 32-bit CRC signature (8 hex characters). This signature is normally appended onto the data, and then checked when the data is read. If the CRC-32 check differs from the stored value, there is likely to be an error in the data.", 0x75247dbf)]
-        [InlineData("ÊÇÑÊ´Õ¤¹ä·Â", 0x05399b9e)]
-        [InlineData("¡¢¤§", 0x48839247)]
-        [InlineData("¢¤§¨", 0x48839247)]
+        [InlineData("ÊÇÑÊ´Õ¤¹ä·Â", 0x5fc1be2c)]
+        [InlineData("¡¢¤§", 0x5d8914f4)]
+        [InlineData("¢¤§¨", 0x576428b2)]
         public void Test3(string text, uint expected)
         {
             var sut = new Crc32Dotnet();
-            //var bytes = Encoding.UTF8.GetBytes(text);
-            var bytes = Encoding.ASCII.GetBytes(text);
+            var bytes = Encoding.UTF8.GetBytes(text);
             var actual = sut.Get(bytes);
             Assert.Equal(expected, actual);
         }
